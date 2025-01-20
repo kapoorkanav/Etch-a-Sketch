65ecc61192a034b6eefd04f68d4d110d4b9560c6
Kanav Kapoor <142036854+kapoorkanav@users.noreply.github.com>
2025-01-20 14:08:21 +0530

    Make html, css, js files and code for etch sketch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+This webpage will allow users to draw images of their choice by hovering over the grids in the workspace.
+Application written in html, css, js.
---
+++ script.js
@@ -0,0 +1,29 @@
+let updates=1;
+function makeGrid(n){
+    const grids=document.querySelector(".grids");
+    grids.textContent="";
+    updates=0;
+    for(let i=0;i<n*n;i++){
+        const innerGrid=document.createElement("div");
+        let size=720/n;
+        innerGrid.style.height=`${size}px`;
+        innerGrid.style.width=`${size}px`;
+        innerGrid.className="grid";
+        innerGrid.addEventListener("mouseleave",()=>{
+            innerGrid.style.backgroundColor='#'+Math. floor(Math. random()*16777215). toString(16);
+            // innerGrid.style.backgroundColor=white;
+            if(updates<10) innerGrid.style.opacity=updates/10;
+            updates++;
+        })
+        console.log("adding");
+        grids.appendChild(innerGrid);
+    }
+}
+makeGrid(4);
+
+const btn=document.getElementById("btn");
+btn.addEventListener("click",()=>{
+    let newSize=prompt("Enter new size of grid (n):");
+    if(newSize<=100&&newSize>=1)
+    makeGrid(newSize);
+});
